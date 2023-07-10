8179a342b2af7e47b0b4d9561022269342f65168
Alexandra <alexandra.nedomolkina@office.e2e4gu.ru>
2023-07-10 12:19:14 +0700

    add navigation in layout

--- cinema-app/src/app/navigation.tsx
+++ cinema-app/src/app/navigation.tsx
@@ -1,10 +1,16 @@
 import logo from '../app/images/logo.png'
 import Image from 'next/image'
+import styles from './page.module.css'
 
 export default function Navigation() {
-    return (<><Image
-    src={logo}
-    alt="LOGO">
-    </Image>
+    return (<>
+        <div className={styles.nav}><Image
+            src={logo}
+            width={280}
+            height={48}
+            alt="LOGO">
+        </Image>
+            <button className={styles.btn_in}>Войти</button>
+        </div>
     </>)
-  }
+}
