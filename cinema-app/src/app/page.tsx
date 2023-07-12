ed1360b1ce6716e25d11deda3aa9ec0a5697d48f
Alexandra <alexandra.nedomolkina@office.e2e4gu.ru>
2023-07-12 16:28:40 +0700

    button for description

--- cinema-app/src/app/page.tsx
+++ cinema-app/src/app/page.tsx
@@ -1,12 +1,15 @@
+'use client'
 import styles from './page.module.css'
 import Image from 'next/image'
 import witcher from './images/witcher.jpg'
+import { useRouter } from 'next/navigation'
 
 const mainImageStyle = {
   borderRadius: '8px',
 }
 
 export default function Home() {
+  const router = useRouter()
   return (
     <main className={styles.main}>
       <div className={styles.container}>
@@ -24,6 +27,10 @@ export default function Home() {
           <span className={styles.subtitle}>Окунись в мир кино</span>
           <br />
           <span className={styles.subscription}>В нашем кинотеатре вы проведёте незабываемые вечера со своими любимыми героями. На сайте представлен широкий выбор фильмов разных жанров. Заказ и бронирование билетов онлайн.</span>
+          <div className={styles.btn_group}>
+          <button className={styles.landing_btn_buy} onClick={() => router.push('/pages/affiche')}>Купить билет</button>
+          <button className={styles.landing_btn_watch}>Смотреть ролик</button>
+          </div>
         </div>
       </div>
     </main>
