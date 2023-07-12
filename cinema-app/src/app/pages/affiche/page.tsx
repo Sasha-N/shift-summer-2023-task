2dc4c48cd9e71d88bb1fde64149db4bab192fa31
Alexandra <alexandra.nedomolkina@office.e2e4gu.ru>
2023-07-12 20:11:06 +0700

    title subtitle for affiche

--- cinema-app/src/app/pages/affiche/page.tsx
+++ cinema-app/src/app/pages/affiche/page.tsx
@@ -2,6 +2,7 @@ import styles from './page.module.css'
 
 export default function Affiche() {
     return <>
-    <h2 className={styles.title}>АФИША</h2>
+    <h3 className={styles.title}>АФИША</h3>
+    <h4 className={styles.subtitle}><a className={styles.link}>сегодня</a> в прокате</h4>
     </>
   }
