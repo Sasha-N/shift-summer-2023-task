16b928060b917839f18956176e2c988511979767
Alexandra <alexandra.nedomolkina@office.e2e4gu.ru>
2023-07-12 22:05:23 +0700

    apollo server

--- cinema-app/src/app/pages/affiche/page.tsx
+++ cinema-app/src/app/pages/affiche/page.tsx
@@ -1,8 +1,53 @@
+'use client'
 import styles from './page.module.css'
+import { gql, useQuery } from "@apollo/client";
+
+const GET_FILMS = gql`query getCinemaToday {
+  getCinemaToday {
+    films {
+      actors {
+        fullName
+        id
+        professions
+      }
+    ageRating
+    country {
+      code
+      code2
+      id
+      name
+    }
+    description
+    directors {
+      fullName
+      id
+      professions
+    }
+    genres
+    id
+    img
+    name
+    originalName
+    releaseDate
+    runtime
+    userRatings {
+      imdb
+      kinopoisk
+    }
+  }
+  reason
+  success
+  }
+} 
+`
 
 export default function Affiche() {
-    return <>
+  const allFilms = useQuery(GET_FILMS);
+  if (allFilms.data){
+    console.log(allFilms.data)
+  }
+  return <>
     <h3 className={styles.title}>АФИША</h3>
     <h4 className={styles.subtitle}><a className={styles.link}>сегодня</a> в прокате</h4>
-    </>
-  }
+  </>
+}
