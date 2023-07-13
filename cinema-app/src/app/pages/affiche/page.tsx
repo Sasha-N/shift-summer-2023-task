d5f2ee2896aa7b280c397fe331e9e82361040b31
Alexandra <alexandra.nedomolkina@office.e2e4gu.ru>
2023-07-13 19:47:49 +0700

    draft film card

--- cinema-app/src/app/pages/affiche/page.tsx
+++ cinema-app/src/app/pages/affiche/page.tsx
@@ -43,11 +43,28 @@ const GET_FILMS = gql`query getCinemaToday {
 
 export default function Affiche() {
   const allFilms = useQuery(GET_FILMS);
-  if (allFilms.data){
+  let genre = '';
+  let release = '';
+  let name = '';
+  if (allFilms.data) {
     console.log(allFilms.data.getCinemaToday.films)
+    genre = allFilms.data.getCinemaToday.films[0].genres[0];
+    release = allFilms.data.getCinemaToday.films[0].releaseDate;
+    name = allFilms.data.getCinemaToday.films[0].name
   }
   return <>
     <h3 className={styles.title}>АФИША</h3>
     <h4 className={styles.subtitle}><a className={styles.link}>сегодня</a> в прокате</h4>
+    <div className={styles.container_card}>
+      <div className={styles.label}><p className={styles.genre}>{genre}</p><p className={styles.release}>{release}</p></div>
+      <div className={styles.cover}></div>
+      <div className={styles.description}>
+        <p className={styles.name}>{name}</p>
+        <p className={styles.original_name}>{name}</p>
+        <p className={styles.rating}>IMDB: {name}</p>
+        <p className={styles.rating}>Kinopoisk - {name}</p>
+        <button className={styles.detail}>Подробнее</button>
+      </div>
+    </div>
   </>
 }
